1043cc33ea2e8b85525bee805639c64bbd0979ba
FunnyBoyZD <alexskipka@gmail.com>
2024-01-02 21:13:37 +0200

    Added SQL selects with using different strategies of selecting data

--- Motosalon/Data/DataService.cs
+++ Motosalon/Data/DataService.cs
@@ -11,9 +11,9 @@ namespace Motosalon.Data
     {
         public static List<Client> GetClients()
         {
-            using(MotoContext db = new MotoContext())
+            using (MotoContext db = new MotoContext())
             {
-                return db.Clients.Include(c=>c.BoughtMoto).ToList();
+                return db.Clients.Include(c => c.BoughtMoto).ToList();
             }
         }
         public static void AddClient(Client client)
@@ -53,6 +53,81 @@ namespace Motosalon.Data
             {
                 db.Mototransports.Remove(mototransport);
                 db.SaveChanges();
+            }
+        }
+
+        public static List<Mototransport> GetMototransportsWithPriceOver100000()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Mototransports.Where(m => m.Price > 100000).ToList();
+            }
+        }
+
+        public static List<Client> GetClientsWhoBoughtHondaOrYamaha()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return (List<Client>)db.Clients.Union(
+                    db.Clients.Where(c => c.BoughtMoto.Brand == "Yamaha").ToList());
+            }
+        }
+
+        public static List<Client> GetClientsWhoBoughtHondaButNotYamaha()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Clients.Except(
+                    db.Clients.Where(c => c.BoughtMoto.Brand == "Yamaha")).ToList();
+            }
+        }
+
+        public static List<Client> GetClientsWhoBoughtHondaAndYamaha()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Clients.Intersect(
+                    db.Clients.Where(c => c.BoughtMoto.Brand == "Yamaha")).ToList();
+            }
+        }
+
+        public static Dictionary<string, int> GetMototransportCountByBrand()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Mototransports
+                    .GroupBy(m => m.Brand)
+                    .ToDictionary(g => g.Key, g => g.Count());
+            }
+        }
+
+        public static List<Client> GetClientsWithMototransport()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Clients
+                    .Include(c => c.BoughtMoto) // Eager loading
+                    .ToList();
+            }
+        }
+
+        public static Client GetClientWithMototransport(int clientId)
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Clients
+                    .Include(c => c.BoughtMoto) // Eager loading
+                    .FirstOrDefault(c => c.Id == clientId);
+            }
+        }
+
+        public static List<Client> GetClientsWithoutMototransport()
+        {
+            using (MotoContext db = new MotoContext())
+            {
+                return db.Clients
+                    .Where(c => c.BoughtMoto.Clients.Count == 0) // Explicit loading
+                    .ToList();
             }
         }
     }
